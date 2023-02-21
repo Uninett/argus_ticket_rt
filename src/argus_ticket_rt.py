b30f20d96ffedc144c1c2d1a9337aa6619d237f9
Johanna England <johanna.england@sikt.no>
2023-02-21 12:35:49 +0100

    Check for status code in unexpected response error

--- src/argus_ticket_rt.py
+++ src/argus_ticket_rt.py
@@ -125,10 +125,21 @@ class RequestTrackerPlugin(TicketPlugin):
             authentication_error = "Request Tracker: The authentication details are incorrect. Please check and update the setting 'TICKET_AUTHENTICATION_SECRET'."
             LOG.exception(authentication_error)
             raise TicketSettingsException(authentication_error)
-        except (rt_exceptions.NotAllowedError, rt_exceptions.UnexpectedResponseError):
+        except rt_exceptions.NotAllowedError:
             permission_error = "Request Tracker: Authenticated client does not have sufficient permissions."
             LOG.exception(permission_error)
             raise TicketCreationException(permission_error)
+        except rt_exceptions.UnexpectedResponseError as e:
+            if e.status_code == 403:
+                permission_error = "Request Tracker: Authenticated client does not have sufficient permissions."
+                LOG.exception(permission_error)
+                raise TicketCreationException(permission_error)
+            unexpected_error = (
+                f"Request Tracker: An unexpected response was encountered: {e.message}."
+            )
+            LOG.exception(unexpected_error)
+            raise TicketCreationException(unexpected_error)
+
         except rt_exceptions.NotFoundError:
             queue_error = f"Request Tracker: No queue with the name {queue} can be found. Please check and update the setting 'TICKET_INFORMATION'."
             LOG.exception(queue_error)
