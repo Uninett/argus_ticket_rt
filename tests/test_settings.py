8adc3a44bf28c508694233e37baa95b0212d5c9c
Johanna England <johanna.england@sikt.no>
2026-08-31 14:32:57 +0200

    Update settings to get tests to work

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,15 +5,60 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 AUTH_USER_MODEL = "argus_auth.User"
 
 USE_TZ = True
+TIME_ZONE = "Europe/Oslo"
 
 INSTALLED_APPS = [
-    "django.contrib.contenttypes",
+    # overrides others. must come first
+    "argus.htmx",
+    # Django 1st party apps
+    "django.contrib.admin",
     "django.contrib.auth",
-    # Must make tables
+    "django.contrib.contenttypes",
+    "django.contrib.postgres",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.forms",
+    # 3rd party apps
+    "django_tasks",
+    "django_tasks_db",
+    "corsheaders",
+    "social_django",
+    "rest_framework",
     "rest_framework.authtoken",
+    "drf_spectacular",
+    "django_filters",
+    "phonenumber_field",
+    "knox",  # token auth
+    "django_htmx",
+    "widget_tweaks",
+    "fontawesomefree",
+    # Argus apps
     "argus.auth",
+    "argus.base",
     "argus.incident",
+    "argus.filter",
     "argus.notificationprofile",
+    "argus.dev",
+    "argus.plannedmaintenance",
+    "argus.versioncheck",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social_django.middleware.SocialAuthExceptionMiddleware",
+    "django.contrib.auth.middleware.RemoteUserMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
+    "argus.htmx.middleware.LoginRequiredMiddleware",
+    "argus.htmx.middleware.HtmxMessageMiddleware",
 ]
 
 DATABASES = {
@@ -24,6 +69,28 @@ DATABASES = {
 }
 
 MIDDLEWARE_CLASSES = ()
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "debug": True,
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
+                "argus.auth.context_processors.preferences",
+                "argus.htmx.context_processors.static_paths",
+                "argus.htmx.context_processors.metadata",
+                "argus.htmx.context_processors.banner_message",
+            ],
+        },
+    }
+]
 
 # Argus specific settings
 MEDIA_PLUGINS = [
